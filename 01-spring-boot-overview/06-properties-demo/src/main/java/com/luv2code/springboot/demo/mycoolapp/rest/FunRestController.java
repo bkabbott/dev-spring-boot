8b03141622543a5238f8274e389eb4d4b19c982f
Brian Abbott <hi@brianabbott.com>
2026-06-21 17:36:37 -0400

    inject application properties into controller members

--- 01-spring-boot-overview/06-properties-demo/src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
+++ 01-spring-boot-overview/06-properties-demo/src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -1,10 +1,23 @@
 package com.luv2code.springboot.demo.mycoolapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+
+    // inject properties for: coach.name and team.name
+    @Value("${coach.name}")
+    private String coachName;
+    @Value("${team.name}")
+    private String teamName;
+
+    // expose new endpoint for "teaminfo"
+    @GetMapping("/teaminfo")
+    public String getTeamInfo() {
+        return String.format("Coach Name: %s, Team Name: %s", coachName, teamName);
+    }
 
     // expose "/" that return "Hello World"
     @GetMapping("/")
