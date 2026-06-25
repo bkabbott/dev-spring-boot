6a245d61f3062f01fff4232923482bda700deb28
Brian Abbott <hi@brianabbott.com>
2026-06-25 10:38:43 -0400

    use @Qualifier to specify which implementiation of an instance will be used

--- 02-spring-boot-spring-core/04-qualifiers/src/main/java/com/luv2code/springcoredemo/common/TrackCoach.java
+++ 02-spring-boot-spring-core/04-qualifiers/src/main/java/com/luv2code/springcoredemo/common/TrackCoach.java
@@ -3,10 +3,11 @@ package com.luv2code.springcoredemo.common;
 import org.springframework.stereotype.Component;
 
 @Component
-public class BaeballCoach implements Coach {
+public class TrackCoach implements Coach {
 
     @Override
     public String getDailyWorkout() {
-        return "Spend 30 minutes in batting practice.";
+        return "Run a hard 5k";
     }
+
 }
