0d32cf7610262803ccf0cc27278dcc8a087a9fc1
Brian Abbott <hi@brianabbott.com>
2026-07-01 17:57:35 -0400

    finish 08-bean-lifecycle-methods/, add 09-java-config-bean/

---
+++ 02-spring-boot-spring-core/09-java-config-bean/src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
@@ -0,0 +1,26 @@
+package com.luv2code.springcoredemo.rest;
+
+import com.luv2code.springcoredemo.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    // define a private filed for the dependency
+    private Coach coach;
+
+    @Autowired
+    public DemoController(@Qualifier("cricketCoach") Coach coach) {
+        System.out.println("In constructor " + getClass().getSimpleName());
+        this.coach = coach;
+    }
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout() {
+        return coach.getDailyWorkout();
+    }
+
+}
